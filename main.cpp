c8f41d0a63b6e939090bd110d6dd353ffbbfb2dc
EmelinAA <emelinalekcey@yandex.ru>
2021-04-19 17:02:12 +0300

    code:вывод гистограммы

--- main.cpp
+++ main.cpp
@@ -28,7 +28,6 @@ void find_minmax(const vector<double> &numbers, double& min, double& max)
         }
     }
 }
-
 vector<size_t> make_histogram(const vector<double> &numbers,size_t &bin_count)
 {
     double min, max;
@@ -86,6 +85,58 @@ void   show_histogram_text(vector <size_t> bins)
 
 }
 
+void svg_end()
+{
+    cout << "</svg>\n";
+}
+void
+svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='" << left << "' y='"<<baseline<<"'>"<<text<<"</text>";
+
+}
+void svg_rect(double x, double y, double width, double height, string line_colour, string fil)
+{
+    cout<<"<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"stroke='"<< colour <<"' fill='"<<fil<<"'/>";
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins)
+{
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    double top = 0;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    svg_rect(TEXT_WIDTH, top, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
+    for (size_t bin : bins)
+    {
+        colour=red;
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, ,);
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
+
+
+
 int main()
 {
 
@@ -98,6 +149,6 @@ int main()
     cerr << "Enter column count: ";
     cin >> bin_count;
     const auto bins = make_histogram(numbers, bin_count);
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
     return 0;
 }
