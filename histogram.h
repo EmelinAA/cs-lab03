3f99754c68656314cd1f0ce1fc9fbf18e7553845
E-A-AIA-02-20 <alekceyemelin@yandex.ru>
2021-04-23 17:37:46 +0300

    code: выделение в модули svg функций и функций работы с текстовой гистограммой

--- histogram.h
+++ histogram.h
@@ -4,5 +4,7 @@
 #include <iostream>
 using namespace std;
 void find_minmax(const vector<double>& numbers, double& min, double& max);
-
+vector<double> input_numbers(size_t count);
+vector<size_t> make_histogram(const vector<double>& numbers, size_t& bin_count);
+void   show_histogram_text(vector <size_t> bins);
 #endif // HISTOGRAM_H_INCLUDED
