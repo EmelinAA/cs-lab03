530f66dc75bad2e9c5427429cda54a07ebafd8a1
E-A-AIA-02-20 <alekceyemelin@yandex.ru>
2021-04-23 16:22:20 +0300

    code: добавлены значения цвета и заливки по умолчанию

--- main.cpp
+++ main.cpp
@@ -105,9 +105,9 @@ void svg_text(double left, double baseline, string text)
     cout << "<text x='" << left << "' y='"<<baseline<<"'>"<<text<<"</text>";
 
 }
-void svg_rect(double x, double y, double width, double height, string line_colour, string fil)
+void svg_rect(double x, double y, double width, double height, string line_colour = "black", string fill = "black")
 {
-    cout<<"<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"stroke='"<< colour <<"' fill='"<<fil<<"'/>";
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << line_colour << "' fill='#" << fill << "'/>";
 }
 
 void
@@ -121,15 +121,18 @@ show_histogram_svg(const vector<size_t>& bins)
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
     double top = 0;
+    string colour, fill;
+    cerr << "Enter line colour: ";
+    cin >> colour;
+    cerr << "Enter column fill: ";
+    cin >> fill;
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
-    svg_rect(TEXT_WIDTH, top, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
     for (size_t bin : bins)
     {
-        colour=red;
         const double bin_width = BLOCK_WIDTH * bin;
         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, ,);
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,colour,fill);
         top += BIN_HEIGHT;
     }
     svg_end();
