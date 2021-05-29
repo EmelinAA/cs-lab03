e5da06f99105b5428399217089a2f36c37c95a46
E-A-AIA-02-20 <alekceyemelin@yandex.ru>
2021-05-29 21:19:14 +0300

    code: curl

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 #include "svg.h"
 #include "histogram.h"
+#include <curl/curl.h>
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 using namespace std;
 int main()
 {
+    curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
