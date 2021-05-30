1e254bb0ff6229f7cec615a34348c7c9c6d71bc6
E-A-AIA-02-20 <alekceyemelin@yandex.ru>
2021-05-30 18:51:20 +0300

    code: включен ввод и вывод данных

--- main.cpp
+++ main.cpp
@@ -8,54 +8,9 @@
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 using namespace std;
- string
-    make_info_text()
-    {
-        stringstream buffer;
-        DWORD info = GetVersion();
-        DWORD mask = 0x0000ffff;
-        DWORD platform = info >> 16;
-        DWORD mask2 = 0x000000ff;
-        DWORD version =info & mask;
-        if ((info & 0x80000000) == 0)
-        {
-            DWORD version_minor = version>>8;
-            DWORD version_major = version &mask2;
-            DWORD build = platform;
-            printf("Windows v.%u.%u (build %u) \n",version_major,version_minor,build);
-            char lpBuffer[MAX_COMPUTERNAME_LENGTH + 1];
-            DWORD si = MAX_COMPUTERNAME_LENGTH+1;
-            printf("Computer name: %s\n",lpBuffer);
-            return buffer.str();
-        }
-    }
+
 int main()
 {
-   /* string
-    make_info_text()
-    {
-        stringstream buffer;
-        DWORD info = GetVersion();
-        DWORD mask = 0x0000ffff;
-        DWORD platform = info >> 16;
-        DWORD mask2 = 0x000000ff;
-        DWORD version =info & mask;
-        if ((info & 0xf0000000) == 0)
-        {
-            DWORD version_minor = version>>8;
-            DWORD version_major = version &mask2;
-            DWORD build = platform;
-            printf("Windows v.%u.%u (build %u) \n",version_major,version_minor,build);
-            char lpBuffer[MAX_COMPUTERNAME_LENGTH + 1];
-            DWORD si = MAX_COMPUTERNAME_LENGTH+1;
-            GetComputerNameA(lpBuffer,&si);
-            printf("Computer name: %s\n",lpbuffer);
-            return buffer.str();
-        }
-    }
-    */
-    make_info_text();
-    return (0);
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
--- svg.h
+++ svg.h
@@ -1,15 +1,16 @@
 #ifndef SVG_H_INCLUDED
 #define SVG_H_INCLUDED
 #include <vector>
-#include <string>;
+#include <string>
+#include <windows.h>
+#include <sstream>
 
 using namespace std;
 bool svg_check(double block_width);
 void svg_end();
 void svg_begin(double width, double height);
+string make_info_text();
 void svg_text(double left, double baseline, string text);
 void svg_rect(double x, double y, double width, double height, string line_colour = "black", string fill = "black");
 void show_histogram_svg(const vector<size_t>& bins);
-
-
 #endif // SVG_H_INCLUDED
